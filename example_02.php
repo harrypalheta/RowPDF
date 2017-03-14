91f3d2cb537ca55661e5aef5a4b4be3f224870f0
Harryson Palheta <harryson.palheta@pmm.manaus.am.gov.br>
2017-03-14 13:21:05 -0400

    Ajuste de possibilidades para a configuração do alinhamento.

--- example_02.php
+++ example_02.php
@@ -5,7 +5,7 @@ $pdf=new Row();
 $pdf->AddPage('L');
 $pdf->SetXY(10,10);
  $arrayConfig = [
-            'align'         => ['J','J','C','R'],    // aceita array também
+            'align'         => ['J','L','C','R'],    // aceita array também
             'background'    => true,            // aceita array também
             'border'        => 1,                // aceita array também
             'fill'          => 240, // cinza     // aceita array também
@@ -13,7 +13,7 @@ $pdf->SetXY(10,10);
             'height'        => 5 ,               // só aceita inteiro
             'size'          => 7,                // aceita array também
             'style'         => 'B',              // aceita array também
-            'valign'         => ['T','T','M','B'],   // aceita array também
+            'valign'         => ['','T','M','B'],   // aceita array também
             'width'         => [50,50,50,50],       // só aceita array
             'reset'        => true               // só aceita booleano
          ];
@@ -21,7 +21,7 @@ $pdf->SetXY(10,10);
 $pdf->RowConfig($arrayConfig);
 $pdf->Row([
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    'This sentence is justified in the TOP of the box.',
+    'This sentence is left on the TOP of the box.',
     'This sentence is centered in the MIDDLE of the box.',
     'This sentence is right on the BOTTOM of the box.'
 ]);
